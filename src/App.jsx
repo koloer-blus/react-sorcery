8055c6196e977af723837c76c56521e641615e35
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-16 00:20:00 +0800

    feat(router): hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import PageRoutes from './router';
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter basename="/ReactPress">
+      <HashRouter>
         <PageRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
