8231e64fff563d8c30a82fb735733c8c0f7e86b6
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-15 20:42:09 +0800

    feat(bin): 代码逻辑梳理

--- bin/index.js
+++ bin/index.js
@@ -12,14 +12,10 @@ const PRESS_MD_JSON_PATH = path.resolve(__dirname, "../src/md/");
 const MD_JSON_FILE_TYPE = ".md.json";
 const REGISTRY_COMPONENTS_FILE = path.resolve(__dirname, "../src/registry.jsx");
 const ROUTER_CONFIG_FILE = path.resolve(__dirname, "../src/route.config.json");
-const COMMAND_PATH = process.cwd();
 
-// get the ReactPress info in package.json
-const { version, name, repository, homepage } = pkg;
-
-
-
-const program = new Command();
+// Global Variable Path
+const docs_path = [];
+let count = 1;
 
 function clearTheBuildPath() {
   fs.removeSync(PRESS_MD_JSON_PATH);
@@ -27,12 +23,9 @@ function clearTheBuildPath() {
   fs.removeSync(ROUTER_CONFIG_FILE)
   fs.mkdirSync(PRESS_MD_JSON_PATH);
 }
-clearTheBuildPath();
 
-// Global Variable Path
-const docs_path = [];
-let count = 1;
 
+// >>> get the md docs file path list
 function getDocsFilesList(dir, filesList = []) {
   const files = fs.readdirSync(dir);
   files.forEach((item) => {
@@ -58,11 +51,10 @@ function getDocsFilesList(dir, filesList = []) {
   return filesList;
 }
 
-getDocsFilesList(MD_DOCS_PATH, docs_path);
 
 
 /**
- * 通过filePath读取对应的docs
+ * transform md string to json string and write to the *.md.json files
  * @param {{path: string; title: string; name: string; route:string;}[]} filesList 
  */
 function transformMDtoJSON(filesList) {
@@ -89,31 +81,65 @@ function transformMDtoJSON(filesList) {
   })
 }
 
+
+/**
+ * registry the file
+ * @param {Array<{name: string;file: string;}>} mdList 组件名称
+ */
+function registryComponentsText() {
+  const mdList = docs_path.map(doc => ({
+    name: doc.name,
+    file: doc.name + MD_JSON_FILE_TYPE
+  }));
+  // registry the jsx
+  const imrComponents = mdList
+    .map(md => `import ${md.name} from "md/${md.file}";`)
+    .join("\n");
+  const eprComponents = `export default {
+    ${mdList
+      .map(md => `${md.name}: ${md.name},`)
+      .join("\n")
+    }
+  };`
+  const registryFileData =  `
+    ${imrComponents}
+
+    ${eprComponents}
+  `;
+
+  fs.writeFile(REGISTRY_COMPONENTS_FILE, registryFileData, { flag: "w+" }).then(() => {
+    console.log("✅complete registry components")
+  })
+  
+  // registry the josn
+  fs.writeJson(ROUTER_CONFIG_FILE, {
+    pages: docs_path.map(doc => ({
+      md_key: doc.name,
+      path: doc.route
+    }))
+  }, { flag: "w+" }).then(() => {
+    console.log("✅Complete router registry")
+  }).catch(err => {
+    console.log(`Failed to create route file`);
+    console.error(err);
+  })
+}
+
+clearTheBuildPath();
+getDocsFilesList(MD_DOCS_PATH, docs_path);
 transformMDtoJSON(docs_path);
+registryComponentsText(pageNamesList);
+
+// program commands
+const COMMAND_PATH = process.cwd();
+
+// get the ReactPress info in package.json
+const { version, name, repository, homepage } = pkg;
+
+const program = new Command();
 
 
-const pageNamesList = docs_path.map(doc => ({
-  name: doc.name,
-  file: doc.name + MD_JSON_FILE_TYPE
-}));
 
-const registryFileData = registryComponentsText(pageNamesList);
-
-fs.writeFile(REGISTRY_COMPONENTS_FILE, registryFileData, { flag: "w+" }).then(() => {
-  console.log("✅complete registry components")
-})
-
-fs.writeJson(ROUTER_CONFIG_FILE, {
-  pages: docs_path.map(doc => ({
-    md_key: doc.name,
-    path: doc.route
-  }))
-}, { flag: "w+" }).then(() => {
-  console.log("✅Complete router registry")
-}).catch(err => {
-  console.log(`Failed to create route file`);
-  console.error(err);
-})
 
 // >>> output version
 program
